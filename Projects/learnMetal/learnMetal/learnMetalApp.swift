89bcbe8779e130b5180a8cc893c1e34d7b9fcad1
Opunundo <hyeok3822@gmail.com>
2025-08-13 20:16:49 +0900

    settings: 프로젝트 생성

---
+++ Projects/learnMetal/learnMetal/learnMetalApp.swift
@@ -0,0 +1,17 @@
+//
+//  learnMetalApp.swift
+//  learnMetal
+//
+//  Created by Hyeok Cho on 8/13/25.
+//
+
+import SwiftUI
+
+@main
+struct learnMetalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Projects/learnMetal/learnMetalTests/learnMetalTests.swift
@@ -0,0 +1,16 @@
+//
+//  learnMetalTests.swift
+//  learnMetalTests
+//
+//  Created by Hyeok Cho on 8/13/25.
+//
+
+import Testing
+
+struct learnMetalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
